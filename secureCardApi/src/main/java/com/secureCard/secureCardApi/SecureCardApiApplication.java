ddbfff47ff4b8377e84938bba573fbb46aa2b7e9
raissaadev <raissadematossouza@gmail.com>
2026-06-22 21:04:10 -0300

    Initial commit

---
+++ secureCardApi/src/main/java/com/secureCard/secureCardApi/SecureCardApiApplication.java
@@ -0,0 +1,13 @@
+package com.secureCard.secureCardApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureCardApiApplication.class, args);
+	}
+
+}
---
+++ secureCardApi/src/test/java/com/secureCard/secureCardApi/SecureCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureCard.secureCardApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
